18fd9d7cc20de4d1009fb73558e16c7b6607b078
PeterVoerman <peter.jpv@hotmail.com>
2021-12-09 18:10:38 +0100

    Read optimal route from file

--- file_reader.py
+++ file_reader.py
@@ -10,5 +10,20 @@ def read_file(filename):
 
     return x_list, y_list
 
+def read_solution(filename):
+    node_list = []
+
+    with open(filename, "r") as f:
+        print(filename[-17:])
+        if filename[-17:] == 'a280.opt.tour.txt':
+            for line in f.readlines()[4:-1]:
+                node_list.append(int(line))
+        else:
+            for line in f.readlines()[5:-2]:
+                node_list.append(int(line))
+
+    node_list.append(node_list[0])
+    return node_list
+
 # Example usage
-# print(read_file('TSP-Configurations/pcb442.tsp.txt'))
+#print(read_solution('TSP-Configurations/eil51.opt.tour.txt'))
