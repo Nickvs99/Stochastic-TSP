1b47e18ed8d76df7fe18280547237cfd0ac05ce1
PeterVoerman <peter.jpv@hotmail.com>
2021-12-09 17:21:37 +0100

    fixed file reader

--- file_reader.py
+++ file_reader.py
@@ -5,9 +5,9 @@ def read_file(filename):
     with open(filename, "r") as f:
         for line in f.readlines()[6:-1]:
             split_line = line.split(' ')
-            x_list.append(int(split_line[1]))
-            y_list.append(int(split_line[2]))
+            x_list.append(float(split_line[1]))
+            y_list.append(float(split_line[2]))
 
     return x_list, y_list
 
-print(read_file('TSP-Configurations/eil51.tsp.txt'))
+print(read_file('TSP-Configurations/pcb442.tsp.txt'))
