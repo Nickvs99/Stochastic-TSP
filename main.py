d5b2df232220fbaca8466ce6253b96a1fd6382bc
Nick van Santen <vansantennick@gmail.com>
2021-12-15 17:40:21 +0100

    Plot effects of different cooling rates

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-import random
 import numpy as np
+import os
+import pickle
 
 import distance
 from file_reader import read_file, read_solution
-from plot import plot, plot_scores
+from plot import plot, plot_scores, plot_data
 import simulated_annealing
 
 
@@ -21,23 +22,67 @@ def two_opt(order, city_1, city_2):
         new_order[0] = new_order[-1]
     return new_order
 
+def generate_data(a_values, b_values, nruns, dist_table, iterations=10000):
+
+    data = {}
+    best_score = np.inf
+    best_order = None
+    
+    for a in a_values:
+        for b in b_values:
+            print(f"\rCalculating a={a}, b={b}...", end="")
+            score_results = []
+
+            for _ in range(nruns):
+
+                scores, orders = simulated_annealing.run(a, b, dist_table, iterations=iterations)
+                score_results.append(scores)
+
+                min_score = min(scores)
+                if min_score < best_score:
+                    index = scores.index(min_score)
+                    best_score = min_score
+                    best_order = orders[index]
+
+            if a not in data:
+                data[a] = {}
+
+            data[a][b] = score_results
+    print()
+    return data, best_score, best_order
+
+def save_data(filename, data, best_score, best_order):
+    with open(filename, 'wb') as f:
+        pickle.dump([data, best_score, best_order], f)
+
+def load_data(filename):
+    with open(filename, 'rb') as f:
+        return pickle.load(f)
 
 def main():
     filename = 'eil51'
+    cache_filename = f"data/{filename}.pickle"
+    
     x_values, y_values = read_file(f"TSP-Configurations/{filename}.tsp.txt")
-
     dist_table = distance.create_distance_table(x_values, y_values)
-
-    scores, orders = simulated_annealing.run(1000, 10, dist_table, iterations=1000000)
-
-    index = scores.index(min(scores))
-    optimal_route = orders[index]
     
-    print(orders[index])
-    print(scores[index]) 
+    if os.path.isfile(cache_filename):
+        print(f"Loading cached data from {cache_filename}")
+        data, best_score, best_order = load_data(cache_filename)
+    else:
+        print(f"Calculating cached data for {cache_filename}")
+
+        a_values = [1, 10, 100, 1000, 10000]
+        b_values = [1, 10, 100, 1000, 10000]
+        data, best_score, best_order = generate_data(a_values, b_values, 25, dist_table)
+
+        print(f"Saving data to {cache_filename}")
+        save_data(cache_filename, data, best_score, best_order)
+
+    plot_data(data)
     
-    plot_scores(scores)
-    plot(x_values, y_values, optimal_route)
+    print(best_score)
+    plot(x_values, y_values, best_order)
 
 if __name__ == "__main__":
     main()
