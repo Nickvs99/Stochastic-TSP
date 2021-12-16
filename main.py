638b08f67f8da69cd0f879b02b0e0b971e21388b
PeterVoerman <peter.jpv@hotmail.com>
2021-12-16 16:58:53 +0100

    Changed formula

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import random
 import numpy as np
+import matplotlib.pyplot as plt
 
 import distance
 from file_reader import read_file, read_solution
@@ -23,17 +24,26 @@ def two_opt(order, city_1, city_2):
 
 
 def main():
+    n = 1000000
     filename = 'eil51'
     x_values, y_values = read_file(f"TSP-Configurations/{filename}.tsp.txt")
+    route = read_solution(f"TSP-Configurations/{filename}.opt.tour.txt")
 
     dist_table = distance.create_distance_table(x_values, y_values)
 
-    scores, orders = simulated_annealing.run(1, 1, dist_table)
+    afstand = distance.calc_path_length(route, dist_table)
+    print(afstand)
+    scores, orders, temp_list = simulated_annealing.run(50, 20, n, dist_table)
 
     #plot(x_values, y_values, orders[0])
-    print(orders[-1])
-    print(scores[-1])
-    plot(x_values, y_values, orders[-1])
+    print(min(scores))
+    index = scores.index(min(scores))
+    route = orders[index]
+    plot(x_values, y_values, route)
+    plt.plot(np.arange(n), scores)
+    plt.show()
+    plt.plot(np.arange(n-1), temp_list)
+    plt.show()
     
 if __name__ == "__main__":
     main()
--- simulated_annealing.py
+++ simulated_annealing.py
@@ -4,42 +4,49 @@ import random
 import distance
 import main
 
-def run(a, b, dist_table):
-
-    n_cities = 51
+def run(a, b, n, dist_table):
+    temp_list = []
+    n_cities = len(dist_table)
     current_order = [i for i in range(1, n_cities + 1)]
     current_order.append(1)
     current_score = distance.calc_path_length(current_order, dist_table)
+    print(current_score)
     
     orders = [current_order]
     scores = [current_score]
+    last_value = np.inf
 
-    for i in range(1000):
+    for i in range(n-1):
 
 
         edge1 = random.randint(0, n_cities - 1)
         edge2 = random.randint(0, n_cities - 1)
 
-        # TODO edge checks
+        while abs(edge1 - edge2) < 2:
+            edge1 = random.randint(0, n_cities - 1)
+            edge2 = random.randint(0, n_cities - 1)
         
         temp_order = main.two_opt(current_order, edge1, edge2)
         temp_score = distance.calc_path_length(temp_order, dist_table)
 
         temperature = calc_temp(a, b, i)
-        if temp_score < current_score or is_excepted(temp_score, temperature):
+        temp_list.append(temperature)
+        if temp_score < current_score or is_excepted(temp_score, last_value, temperature):
             current_score = temp_score
             current_order = temp_order
 
         scores.append(current_score)
         orders.append(current_order)
 
-    return scores, orders
+        last_value = current_score
+
+    return scores, orders, temp_list
        
-def is_excepted(value, temperature):
-    return random.random() < acceptance_change(value, temperature)     
+def is_excepted(value, last_value, temperature):
+    return random.random() < acceptance_change(value, last_value, temperature)     
 
 def calc_temp(a, b, n):
     return a / np.log(n + b)
 
-def acceptance_change(value, temperature):
-    return np.exp(-value / temperature)
+def acceptance_change(value, last_value, temperature):
+    return np.exp(-(value - last_value) / temperature)
